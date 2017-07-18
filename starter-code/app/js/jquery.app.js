1c5bb44f8fec41c6b1742dd485556342f8279353
mtclark0518 <mtclark0518@gmail.com>
2017-07-18 16:41:02 -0600

    adds window reload on form submit

--- starter-code/app/js/jquery.app.js
+++ starter-code/app/js/jquery.app.js
@@ -4,21 +4,59 @@
 
 
 
-
+var catUl = $('#cats');
 $(document).ready(function(){
-// $('#submit').click(function(event){
-// 	event.preventDefault();
+
+
+var OutgoingCat = function(){
+	this.name = $('#cat-name').val();
+	this.note = $('#cat-note').val();
+	};
+	
+
+$('#submit').click(function(event){
+	event.preventDefault();
+
+	var userCat = new OutgoingCat();
+	var userString = JSON.stringify(userCat);
+	console.log(userString);
+		$.ajax({
+			type: "post",
+			url: "https://ga-cat-rescue.herokuapp.com/api/cats",
+		 	data: userString,
+			success: function(){
+		 		location.reload();
+		 	}
+		 });
+
+
+		});
+	
+
+	// document.createElement('li');
+	// userCats.append(userInput);
+	// catUl.append(userCats);
+	// console.log(userInput);
+
+
+
+
+
+
+
 var thing;
 
 	var kitty = $.get( "https://ga-cat-rescue.herokuapp.com/api/cats")
 		.done(function(data){
 		 thing = JSON.parse(kitty.responseText);
-		 var catUl = $('#cats');
 		 for(i=0; i<thing.length; i++){
 		 var catList = document.createElement("li");
-		 catList.appendChild(thing[i]);
-		 catUl.appendChild(catList);
+
+		 catList.append(thing[i].name + " - " + thing[i].note);
+		 catUl.append(catList);
 		}
 	});
 	
+
+
 });
